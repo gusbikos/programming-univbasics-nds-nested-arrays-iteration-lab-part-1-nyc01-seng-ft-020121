947e19e36c4efcb5789ad3f12186fe9aeb27c024
Gus Bikos <gusbikos@gmail.com>
2021-01-16 20:19:36 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ def find_even_values(src)
   while row_index < src.length do
     element_index = 0
     while element_index < src[row_index].length do
-      p src.[row_index][element_index].even?
+      p src[row_index][element_index].even?
       element_index += 1
     end
     row_index += 1
